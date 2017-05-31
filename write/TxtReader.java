e6a6ced9366d087384216a298e04da65dc733d21
daviddavid97 <zhangxiyinzxy@gmail.com>
2017-05-31 14:30:45 +0800

    1

---
+++ write/TxtReader.java
@@ -0,0 +1,62 @@
+package write;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
+import static write.write.writein;
+
+/**
+ * Created by zxy on 5/31/17.
+ */
+public class TxtReader{
+    // 读取文件指定行。
+    static void readAppointedLineNumber(File sourceFile)
+            throws IOException {
+        int nu = 0;
+        FileReader in = new FileReader(sourceFile);
+        LineNumberReader reader = new LineNumberReader(in);
+        String s = "";
+        int lines = 0;
+        while (s != null && lines < getTotalLines(sourceFile)) {
+            lines++;
+            s = reader.readLine();
+            int len = s.length();
+            if (len > 10) {
+                nu++;
+                s.replace(" ", "");
+                writein(nu);
+                //下面一行准备删除
+                System.out.println(s);
+            }
+        }
+        System.exit(0);
+        reader.close();
+        in.close();
+    }
+    // 文件内容的总行数。
+    static int getTotalLines(File file) throws IOException {
+        FileReader in = new FileReader(file);
+        LineNumberReader reader = new LineNumberReader(in);
+        String s = reader.readLine();
+        int lines = 0;
+        while (s != null) {
+            lines++;
+            s = reader.readLine();
+        }
+        reader.close();
+        in.close();
+        return lines;
+    }
+
+    /**
+     * 读取文件指定行。
+     */
+    public static void main(String[] args) throws IOException {
+        File sourceFile = new File("/home/zxy/test2/大学生毕业论文范文参考.txt");
+        // 读取指定的行
+        readAppointedLineNumber(sourceFile);
+        // 获取文件的内容的总行数
+    }
+}
---
+++ write/write.java
@@ -0,0 +1,10 @@
+package write;
+
+/**
+ * Created by zxy on 5/31/17.
+ */
+public class write {
+    static void writein(int i){
+        System.out.println("w");
+    }
+}
