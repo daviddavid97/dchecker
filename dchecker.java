279d6d4c2b1765c532ba43eeb1c082d07037d07c
daviddavid97 <zhangxiyinzxy@gmail.com>
2017-05-31 20:58:24 +0800

    1

---
+++ dchecker.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import static receive.receive.filereceive;
+
+/**
+ * Created by zxy on 5/31/17.
+ */
+public class dchecker {
+    public static void main(String[] args) throws IOException {
+        filereceive();
+    }
+}
--- receive/receive.java
+++ receive/receive.java
@@ -4,4 +4,7 @@ package receive;
  * Created by zxy on 5/31/17.
  */
 public class receive {
+    public static void filereceive{
+        System.out.println("w");
+    }
 }
