182edef9829e9154d6dceb645960cbc4a1edeffa
daviddavid97 <zhangxiyinzxy@gmail.com>
2017-06-07 14:04:48 +0800

    1

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `latest` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `content` text,
+  `content` longtext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 )ENGINE=MyISAM DEFAULT CHARSET=utf8;
