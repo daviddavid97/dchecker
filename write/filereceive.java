af3f447f6fb0560ebcdd816f2685591f0497ce3f
daviddavid97 <zhangxiyinzxy@gmail.com>
2017-06-07 14:40:51 +0800

    1

--- write/filereceive.java
+++ write/filereceive.java
@@ -3,8 +3,8 @@ package write;
 /**
  * Created by zxy on 6/7/17.
  */
-public class filereceive {
-    public static void receive{
+public class filereceive{
+    public static void receive(){
         System.out.print("w");
     }
 }
